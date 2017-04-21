3219851e701b575148262c65787b7e30f5c20d9e
System Administrator <root@MacBook-Pro-de-imakumo-mac118.local>
2017-04-21 10:32:14 +0200

    Exercise 2Bis

--- src/exercise4.js
+++ src/exercise4.js
@@ -5,15 +5,20 @@ console.log(chalk.cyan('\nEXERCISE 4 LOADED\n'));
 
 var promiseNumbers = new Promise(function (fulfill, reject) {
   setTimeout(fulfill, 200, [100 , 100, 200, 300, 500])
+  return fulfill();
+
 })
 
 var promiseStrings = new Promise(function (fulfill, reject) {
   setTimeout(fulfill, 2000, ['1' , 'un', '2', '3', 'cinq'])
+  return fulfill();
 })
 
 const asyncYolo = (input) => {
 
-  // Your future job begins here ...
+	console.log(_.zip(input));
+	return _.zip(input);
+
 
 }
 
